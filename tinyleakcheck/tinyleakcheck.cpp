aaee9a2e7200e4cdd771ba571de714d4609cd9be
agatha <>
2021-07-03 22:10:20 -0600

    Minor fix to Windows warning in some cases.

--- tinyleakcheck/tinyleakcheck.cpp
+++ tinyleakcheck/tinyleakcheck.cpp
@@ -1,4 +1,4 @@
-#ifdef _WIN32
+#if defined _WIN32 && !defined _CRT_SECURE_NO_WARNINGS
 	//No, Microsoft :P  The standard library is *not* "deprecated" . . .
 	#define _CRT_SECURE_NO_WARNINGS
 #endif
